ec15793ef5247c4b6e02ff93013f0c8bd24efada
Cocaine4ik <kokotkin.alexander@gmail.com>
2018-02-26 15:42:16 +0200

    CapsLock.java - work on it is ended. All checks work right. The result is  according to condition.

--- Tasks/src/Beginner/CaspLock.java
+++ Tasks/src/Beginner/CaspLock.java
@@ -10,11 +10,47 @@ public class CaspLock {
 		
 		Scanner in = new Scanner(System.in); // create new scanner and return link for him
 		
-		for( ; ; ) {
+		for( ; ; ) { // endless loop, that we use to warn user and make some checks.
 			
 			System.out.println("Enter a Latin letter lower register: ");
 			System.out.println();
-			char letter = in.next().charAt(0);
+			String input = in.nextLine();
+			
+			// checking input, if input is empty, warn user about it
+			// and ask to try again, after that - continue loop.
+			
+			if(input.isEmpty()) {
+				System.out.println("You don't enter anything.");
+				continue;
+			}
+			// checking letter length, if length is more then one character, warn user
+			// about it and ask to try again, after that - continue loop.
+			
+			if(input.length() > 1) {
+				System.out.println("U must enter only one letter. Try again. ");
+				continue;
+			}
+			
+			// checking input, if input is not a small letter, warn user about it
+			// and ask to try again, after that - continue loop.
+						
+			if (input.charAt(0) < 97 || input.charAt(0) > 122 ) {
+				System.out.println("The character which u entered is not a small letter."
+				+ " Try again. ");
+				continue;
+			}
+						
+			// Using Unicode ASCII we know that upper register letter code is 32
+			// more then small letter code. Using this we can transform lower register letter
+			// to upper register
+			
+			else {
+				char letter = input.charAt(0); // write first letter from the input to char letter
+				letter -=32; // transform letter in upper register
+				System.out.println("Your letter in upper register: " + letter + ".");
+				break;
+			}
+			
 		}
 		
 	}
