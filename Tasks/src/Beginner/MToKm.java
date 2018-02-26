b15ff87ad9d5d1e6eda1f694c8a4ebc238797e06
Cocaine4ik <kokotkin.alexander@gmail.com>
2018-02-26 17:27:50 +0200

    MToKm - created and working like writing in condition.

--- Tasks/src/Beginner/MToKm.java
+++ Tasks/src/Beginner/MToKm.java
@@ -1,5 +1,53 @@
 package Beginner;
 
-public class MToKm {
+import java.util.Scanner;
 
+// program must transform number in meters, which was entered
+// from the keyboard, to number in kilometers
+
+public class MToKm {
+	public static void main(String args[]) {
+		
+		Scanner in = new Scanner(System.in); // create new scanner and return link for him
+		
+		System.out.println("<<Transformg number from meters to kilometers>>");
+		System.out.println();
+		
+		for( ; ; ) {
+			System.out.println("Enter number of meters: ");
+			String input = in.nextLine();
+			
+		// checking input, if input is empty, warn user about it
+		// and ask to try again, after that - continue loop.
+						
+		if(input.isEmpty()) {
+			System.out.println("You don't enter anything.");
+			continue;
+		}
+		
+		// checking input, if input have any characters, warn user about it
+		// and ask to try again, after that - continue loop.
+		
+		if(!input.matches("[-+]?\\d+")){
+			System.out.println("The number must consists only numerals.");
+			continue;
+		}
+		else {
+			
+			// transform input to integer and write in double variable
+			
+			double m = Integer.parseInt(input); 
+			
+			// transform meters to kilometers because we know that 1 km = 1000m
+			
+			double km = m / 1000;
+			
+			// output the result. To make result easy for watching
+			// we transform double m to integer
+			
+			System.out.println((int)m + " meters are " + km + " kylometers");
+			break;
+		}		
+		}
+	}
 }
