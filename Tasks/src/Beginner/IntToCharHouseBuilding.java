76e9ad78a2b5bd006d0dccd589a3fd001846025d
Cocaine4ik <kokotkin.alexander@gmail.com>
2018-02-27 17:57:10 +0200

    IntToCharHouseBuilding - added the second part of program

--- Tasks/src/Beginner/IntToCharHouseBuilding.java
+++ Tasks/src/Beginner/IntToCharHouseBuilding.java
@@ -1,5 +1,7 @@
 package Beginner;
 
+import java.util.Scanner;
+
 // Code a program which will print a picture, using symbols from the table ASCII.
 // Transform integer to character using java transforming types ability.
 // Using System.out.print - u can make something like that
@@ -24,36 +26,78 @@ public class IntToCharHouseBuilding {
 		// left square bracket - 91, right square bracket - 93
 		// left brace - 123, right brace - 125
 		
+		Scanner in = new Scanner(System.in); // create new scanner and return link for him
+		
 		int roof = 45; // roof
 		int wall = 124; // wall
 		int lw = 91; // left part of the window
 		int rw = 93; // right part of the window
-		int ground = 45;
-		int ld = 45;
+		int ground = 45; // ground
+		int ld = 123; // left door part
+		int rd = 125; // right door part
+		int md = 61; // main door part
+		
+		// The first part of program. Building default house!
+		
+		System.out.print("Building house...");
+		System.out.println();
+		System.out.println();
+		
+		//printing roof
 		
 		for(int i = 0; i < 16; i++) {
 			System.out.print((char)roof);
 		}
 		System.out.println();
 		
-		for(int j = 0; j < 5; j++ ) {
+		// printing 7, 6,5,4,3,2 floors
+		
+		for(int j = 0; j < 6; j++ ) {
 			
-			System.out.print((char)wall);
+			System.out.print((char)wall); // printing left wall
+			
+			// printing windows per floor
+			
 			for(int i = 0; i < 7; i++) {
 				System.out.print((char)lw);
 				System.out.print((char)rw);
 			}
-			System.out.print((char)wall);
+			System.out.print((char)wall); // printing right wall
 			System.out.println();
 		}
-		System.out.print((char)wall);
-		System.out.print("      ");
+		
+		// printing 1 floor
+		
+		System.out.print((char)wall); // printing left wall
+		
+		// printing doors
+		
+		System.out.print("     " + (char)ld);
+		System.out.print((char)md);
+		System.out.print((char)md);
+		System.out.print((char)rd + "     ");
+		
+		System.out.print((char)wall); // printing right wall
+		System.out.println();
+		
+		// printing ground
 		
 		for(int i = 0; i < 16; i++) {
 			System.out.print((char)ground);
 		}
 		System.out.println();
-
+		System.out.println();
+		
+		// The second part of program. Building user house!
+		
+		System.out.println("Now u can build u own house!");
+		System.out.println("Let's choose structures to your house.");
+		System.out.println();
+		
+		System.out.println("Choose your roof material:");
+		System.out.println("Default roof - 45");
+		
+		roof = in.nextInt();
 		
 		
 	}
