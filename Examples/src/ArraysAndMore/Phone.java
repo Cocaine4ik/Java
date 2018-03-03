a5570ce672e9f65c9d1ffa570f8f8650df26d10a
Cocaine <Cocaine@DESKTOP-QDQILMQ>
2018-03-03 22:24:55 +0200

    Searh system - practise

--- Examples/src/ArraysAndMore/Phone.java
+++ Examples/src/ArraysAndMore/Phone.java
@@ -5,7 +5,7 @@ package ArraysAndMore;
 public class Phone {
 	public static void main(String args[]) {
 		String numbers[][] = {
-				{ "Tom", "55-5452" },
+				{ "Tom", "555-5452" },
 				{ "Mary", "555-1278" },
 				{ "Jon", "555-3312" },
 				{ "Rachel", "555-7865" },
@@ -14,13 +14,16 @@ public class Phone {
 		
 		// for use this program we need to give her 1 element
 		//of the command line
+		
 		if(args.length !=1 ) System.out.println("Usage: java Phone <name>");
 		else {
 			for(i = 0; i < numbers.length; i++) {
 				if(numbers[i][1].equals(args[0])) {
-					System.out.println();
+					System.out.println(numbers[i][1] + ":" + numbers[i][1]);
+					break;
 				}
 			}
+			if(i == numbers.length) System.out.println("Name not found.");
 		}
 	} 
 }
---
+++ Practise/src/ClassesAndMethods/SearchSystem.java
@@ -0,0 +1,10 @@
+package ClassesAndMethods;
+
+import java.util.Scanner;
+
+public class SearchSystem {
+	public static void main(String args[]) {
+		
+		Scanner in = new Scanner(System.in);	
+	}
+}
