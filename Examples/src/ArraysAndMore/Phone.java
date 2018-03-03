ddaeee80fc39f66dc1f7e076d237da96edd85d1f
Cocaine <Cocaine@DESKTOP-QDQILMQ>
2018-03-03 13:45:28 +0200

    Phone - code review

--- Examples/src/ArraysAndMore/Phone.java
+++ Examples/src/ArraysAndMore/Phone.java
@@ -1,5 +1,26 @@
 package ArraysAndMore;
 
-public class Phone {
+// Simple automatic phone direction
 
+public class Phone {
+	public static void main(String args[]) {
+		String numbers[][] = {
+				{ "Tom", "55-5452" },
+				{ "Mary", "555-1278" },
+				{ "Jon", "555-3312" },
+				{ "Rachel", "555-7865" },
+		};
+		int i;
+		
+		// for use this program we need to give her 1 element
+		//of the command line
+		if(args.length !=1 ) System.out.println("Usage: java Phone <name>");
+		else {
+			for(i = 0; i < numbers.length; i++) {
+				if(numbers[i][1].equals(args[0])) {
+					System.out.println(x);
+				}
+			}
+		}
+	} 
 }
