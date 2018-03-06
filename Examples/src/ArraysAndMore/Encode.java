75504ce1b78c723ad1c93daecae93d7fef636ef9
Alex <Alex@DESKTOP-JIN5V77>
2018-03-06 17:48:24 +0200

    Encode.java - using operation of subordinate exclusioner XOR for encode and decode messages

---
+++ Examples/src/ArraysAndMore/Encode.java
@@ -0,0 +1,32 @@
+package ArraysAndMore;
+
+// using operation of subordinate exclusioner XOR
+// for encode and decode messages 
+
+public class Encode {
+	public static void main(String args[]) {
+		
+		String msg = "This is the test";
+		String encmsg = "";
+		String decmsg = "";
+		int key = 88;
+		
+		System.out.println("Original message: ");
+		System.out.println(msg);
+		
+		// encode message
+		for(int i = 0; i < msg.length(); i++)
+			// creating encode message string
+			encmsg = encmsg + (char)(msg.charAt(i) ^ key);
+		System.out.println("Encoded message: ");
+		System.out.println(encmsg);
+
+		// decode message
+		for(int i = 0; i < msg.length(); i++)
+			// creating decode message string
+			decmsg = decmsg + (char)(encmsg.charAt(i) ^ key);
+		System.out.println("Decoded message: ");
+		System.out.println(decmsg);
+			
+	}
+}
