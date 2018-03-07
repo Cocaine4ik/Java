31666200deccc50bac447605e5cd3dcafea83d0e
Alex <Alex@DESKTOP-JIN5V77>
2018-03-07 17:45:51 +0200

    ShowBits.java have troubles

--- Exercises/src/ArraysAndMore/ShowBitsDemo.java
+++ Exercises/src/ArraysAndMore/ShowBitsDemo.java
@@ -5,5 +5,10 @@ package ArraysAndMore;
 public class ShowBitsDemo {
 	public static void main(String args[]) {
 		
+	ShowBits b = new ShowBits(8);	
+	ShowBits i = new ShowBits(32);
+	ShowBits li = new ShowBits(64);
+	
+	System.out.println("123 in binary:");
 	}
 }
