bda9aa1f741d5a50222c22967b377abc54b90a77
Alex <Alex@DESKTOP-JIN5V77>
2018-03-07 12:44:14 +0200

    ShowBits.java - class which will show valuables in binary code

---
+++ Exercises/src/ArraysAndMore/ShowBits.java
@@ -0,0 +1,37 @@
+package ArraysAndMore;
+
+// class which will show valuables in binary code
+
+public class ShowBits {
+	int numbits;	
+	
+	// construct of the class ShowBits give us the opportunity to create
+	// objects which will show needed value of bits
+	
+	ShowBits(int n) {
+		numbits = n;
+	}
+	
+	void show(long val) {
+		long mask = 1;
+		
+		//move left value to position that we need
+		mask <<= numbits - 1;
+		int spacer = 0;
+		
+		for(; mask != 0; mask >>>=0) {
+			if((val & mask) != 0)
+				System.out.print("1");
+			else
+				System.out.print("0");
+			
+			spacer++;
+		if((spacer % 8) == 0) {
+			System.out.print(" ");
+			spacer = 0;
+		}
+			
+		}
+		System.out.println();
+	}
+}
---
+++ Exercises/src/ArraysAndMore/ShowBitsDemo.java
@@ -0,0 +1,9 @@
+package ArraysAndMore;
+
+// show how ShowBits is working
+
+public class ShowBitsDemo {
+	public static void main(String args[]) {
+		
+	}
+}
