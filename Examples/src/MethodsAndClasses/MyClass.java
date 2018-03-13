868667056f458dea1f620c4ddb69374f2e0efb33
Alex <Alex@DESKTOP-JIN5V77>
2018-03-13 10:46:48 +0200

    MyClass.java - wrote

--- Examples/src/MethodsAndClasses/MyClass.java
+++ Examples/src/MethodsAndClasses/MyClass.java
@@ -1,5 +1,17 @@
 package MethodsAndClasses;
 
-public class MyClass {
+// Public and private access to the class members
 
+public class MyClass {
+	
+	private int alpha; // private access
+	public int beta; // public access
+	int gamma; // public access (default)
+	
+	// Access methods to the variable alpha. 
+	// Class members can apply to the private members of the same class.
+	
+	void setAlpha (int a) {alpha = a; }
+	int getAlpha () {return alpha; }
+	
 }
