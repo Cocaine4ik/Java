36789d54a0dd634f2a5d6990dcbbb99781b1bcfd
Alex <Alex@DESKTOP-JIN5V77>
2018-03-13 10:55:36 +0200

    AccessDemo.java - completed

--- Examples/src/MethodsAndClasses/AccessDemo.java
+++ Examples/src/MethodsAndClasses/AccessDemo.java
@@ -3,5 +3,21 @@ package MethodsAndClasses;
 public class AccessDemo {
 	public static void main(String args[]) {
 		
+		MyClass ob = new MyClass();
+		
+		// Access to the variable alpha is possible only with
+		// help of special created for this target methods.
+		
+		ob.setAlpha(-99);
+		System.out.println("ob.Alpha is: " + ob.getAlpha());
+		
+		// Apply to the variable alpha how is showing next is impossible.
+		// ob.alpha =10;
+		// Error! Alpha is private variable
+		
+		// next applies is possible, because variables
+		// beta and gamma are public
+		ob.beta = 88;
+		ob.gamma  = 10;
 	}
 }
