add33ef7a58cf2aaf7c4cf422dee7ef65feadced
Alex <Alex@DESKTOP-JIN5V77>
2018-03-14 16:00:28 +0200

    new project

---
+++ Linkcom/src/OltTree/OltTree.java
@@ -0,0 +1,19 @@
+package OltTree;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+// OLT Tree map main class
+
+public class OltTree {
+	public static void main(String args[]) {
+		
+		Scanner in = new Scanner(System.in);
+	
+		int count;
+		
+		ArrayList <Integer> OLT = new ArrayList <>(8); // OLT has 8 PONs
+		ArrayList <Integer> PON = new ArrayList <>(32); // PON has 32 slots
+		
+	}
+}
