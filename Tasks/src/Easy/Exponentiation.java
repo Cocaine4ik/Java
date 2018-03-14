646f43eb4db31f4790bb322d6d156b40bc2266f7
Alex <Alex@DESKTOP-JIN5V77>
2018-03-14 17:39:42 +0200

    Exponentiation.java - new class created

---
+++ Tasks/src/Easy/Exponentiation.java
@@ -0,0 +1,30 @@
+package Easy;
+
+import java.util.Scanner;
+
+// Exponentiate number, entered from the keyboard, in power n,
+// power is entered from the keyboard too.
+// Exponentiation must be organize with loops.
+
+public class Exponentiation {
+	public static void main(String args[]) {
+		
+		Scanner in = new Scanner(System.in);
+		int n, pow, number;
+		
+		
+		System.out.print("Enter number:" );
+		n = in.nextInt();
+		
+		System.out.print("Enter power:" );
+		pow = in.nextInt();
+		
+		for(int i = 1; i < pow; i++) {
+			
+			number = 
+		
+			
+		}
+		System.out.println(n);
+	}
+}
