dc429a11ba36f2eb81a52f3d14d288813f0ff2ae
Alex <Alex@DESKTOP-JIN5V77>
2018-03-16 14:43:07 +0200

    ErrMsg.java - test for ErrorMsg

---
+++ Examples/src/MethodsAndClasses/ErrMsg.java
@@ -0,0 +1,12 @@
+package MethodsAndClasses;
+
+// test for ErrorMsg
+
+public class ErrMsg {
+	public static void main(String args[]) {
+		ErrorMsg err = new ErrorMsg();
+		
+		System.out.println(err.getErrorMsg(3));
+		System.out.println(err.getErrorMsg(19));
+	}
+}
