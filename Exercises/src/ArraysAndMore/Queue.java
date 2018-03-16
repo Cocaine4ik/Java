7fad803265cc55f2150be96606b097e5b97fb0db
Alex <Alex@DESKTOP-JIN5V77>
2018-03-16 14:19:21 +0200

    upd

--- Exercises/src/ArraysAndMore/Queue.java
+++ Exercises/src/ArraysAndMore/Queue.java
@@ -1,34 +1,35 @@
 package ArraysAndMore;
-// class which realize the queue for arrays storage
+
+// Improved queue class for storage characters values
+
 public class Queue {
+
+	// next members of class is being closed for now
 	
-	char q[]; // Array to storage elements in queue	
-	int putloc, getloc;
+	private char q[];
+	private int putloc, getloc; // indexes of placement and retrieval elements of queue
 	
-	// indexes of placement and extracting  queue elements
+	Queue(int size) {
+		q = new char[size + 1]; // pick memory for queue
+		putloc = getloc = 0;
+	}
 	
-	Queue(int size) { q = new char[size+1];
-	
-	// allocate memory for queue
-	
-	putloc = getloc = 0; }
-	
-	// put symbol in queue
+	// put character in queue
 	
 	void put(char ch) {
-		if(putloc==q.length-1) {
+		if(putloc == q.length - 1) {
 			System.out.println(" - Queue is full.");
-			return; }
-		putloc++;
-		q[putloc] = ch; }
-	
-	// extract symbol from the queue
-	
-	char get() { 
-		if(getloc == putloc) { 
-			System.out.println(" - Queue is empty."); 
-			return (char) 0; } 
-		getloc++;
-		return q[getloc]; 
+				return;
 		}
+		putloc++;
+		q[putloc] = ch;
+	}
+	char get() {
+		if(getloc == putloc)  {
+			System.out.println(" - Queue is empty.");
+				return (char) 0;
+		}
+        getloc++;
+        return q[getloc];
+	}
 }
