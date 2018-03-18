34e359e5e725da71937d112d5ad64fda4a9b4600
Cocaine <Cocaine@DESKTOP-QDQILMQ>
2018-03-18 12:23:38 +0200

    methods overload

---
+++ Examples/src/MethodsAndClasses/Overload.java
@@ -0,0 +1,33 @@
+package MethodsAndClasses;
+
+// methods overload
+
+public class Overload {
+	
+	// the first method variant
+	
+	void ovlDemo() {
+		System.out.println("No parameters");
+	}
+	
+	// overload method ovlDemo with 1 option  type int
+	// the second variant of method
+	
+	void ovlDemo(int a) {
+		System.out.println("No parameters" + a);
+	}
+	
+	// overload method ovlDemo with 2 options  type int
+	// the third method variant
+	
+	void ovlDemo(int a, int b) {
+		System.out.println("No parameters" + a + " " + b);
+	}
+	
+	// overload method ovlDemo with 2 options  type double
+	// the fourth method variant
+	
+	void ovlDemo(double a, double b) {
+		System.out.println("No parameters" + a + " " + b);
+	}
+}
