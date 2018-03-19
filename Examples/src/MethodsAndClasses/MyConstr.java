fe2330e9d53b5b0d15eb0d13cb82a4ba79c76ca4
Alex <Alex@DESKTOP-JIN5V77>
2018-03-19 17:39:20 +0200

    demonstration on constructors overload

---
+++ Examples/src/MethodsAndClasses/MyConstr.java
@@ -0,0 +1,30 @@
+package MethodsAndClasses;
+
+// demonstration on constructors overload
+
+public class MyConstr {
+	
+	int x;
+	
+	// constructors can overload in different ways
+	
+	MyConstr() {
+		System.out.println("Inside MyConstr().");
+	    x = 0 ;
+	}
+	
+	MyConstr(int i) {
+        System.out.println("Inside MyConstr().");
+        x = 0 ;
+	}
+	MyConstr(double d) {
+        System.out.println("Inside MyConstr(double).");
+        x = (int) d;
+	}
+	
+    MyConstr(int i, int j) {
+        System.out.println("Inside MyConstr(int, int).");
+        x = i * j;
+    }
+
+	}
