8b46770f3efe7d53229977094e256ccb8cad767b
Alex <Alex@DESKTOP-JIN5V77>
2018-03-22 12:06:50 +0200

    // static method usage demo

---
+++ Examples/src/MethodsAndClasses/SDemo2.java
@@ -0,0 +1,17 @@
+package MethodsAndClasses;
+
+// static method usage demo
+
+public class SDemo2 {
+	public static void main(String args[]) {
+		
+		  System.out.println("val is " + StaticMeth.val);
+	        System.out.println("StaticMeth.valDiv2(): " +
+	                            StaticMeth.valDiv2());
+
+	        StaticMeth.val = 4;
+	        System.out.println("val is " + StaticMeth.val);
+	        System.out.println("StaticMeth.valDiv2(): " +
+	                            StaticMeth.valDiv2());
+	}
+}
