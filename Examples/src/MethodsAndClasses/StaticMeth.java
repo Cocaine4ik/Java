274044adc297af955786eb9459abbf6e0fa966e7
Alex <Alex@DESKTOP-JIN5V77>
2018-03-22 12:03:52 +0200

    static method usage

---
+++ Examples/src/MethodsAndClasses/StaticMeth.java
@@ -0,0 +1,16 @@
+package MethodsAndClasses;
+
+// static method usage
+
+public class StaticMeth {
+
+	static int val = 1024; // static variable
+	
+	// static method
+	
+	static int valDiv2() {
+		
+		return val/2;
+	}
+	
+}
