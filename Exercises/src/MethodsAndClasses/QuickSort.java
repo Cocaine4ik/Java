74c218f2c0b1c6ba718cd3a6c44c655327d08ac8
Alex <Alex@DESKTOP-JIN5V77>
2018-03-22 15:45:39 +0200

    upd

---
+++ Exercises/src/MethodsAndClasses/QuickSort.java
@@ -0,0 +1,14 @@
+package MethodsAndClasses;
+
+// Example number 6.3. Simple version of class Quicksort,
+// which realize quick sort
+
+public class QuickSort {
+
+	// organize calling the specific method of quick sort
+	
+	static void qsort(char items[]) {
+		qs(items, 0, items.length-1);
+	}		
+	
+}
