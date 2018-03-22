66de9911e892b998c695df30e92be8a76be833ad
Alex <Alex@DESKTOP-JIN5V77>
2018-03-22 09:19:08 +0200

    upd

--- Examples/src/MethodsAndClasses/Recursion.java
+++ Examples/src/MethodsAndClasses/Recursion.java
@@ -6,5 +6,11 @@ public class Recursion {
 		Factorial f = new Factorial();
 		
 		System.out.println("Factorial of 3 is " + f.factR(3));
+		System.out.println("Factorial of 5 is " + f.factR(5));
+		System.out.println("Factorial of 7 is " + f.factR(7));
+		
+		System.out.println("Factorial of 3 is " + f.factI(3));
+		System.out.println("Factorial of 5 is " + f.factI(6));
+		System.out.println("Factorial of 7 is " + f.factI(7));
 	}
 }
