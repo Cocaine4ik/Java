3304c57555e6c5236046f176bb8bf3a4b7cc71fc
Alex <Alex@DESKTOP-JIN5V77>
2018-03-23 12:39:33 +0200

    inner class usage

--- Exercises/src/MethodsAndClasses/QSDemo.java
+++ Exercises/src/MethodsAndClasses/QSDemo.java
@@ -3,11 +3,21 @@ package MethodsAndClasses;
 public class QSDemo {
 	public static void main(String args[]) {
 		
-		char a[] = {'d', 'x', 'a', 'r', 'p', 'j', 'i'};
+		char a[] = { 'd', 'X', 'a', 'r', 'p', 'j', 'i' };
 		int i;
 		
 		System.out.print("Original array: ");
         for(i=0; i < a.length; i++)
             System.out.print(a[i]) ;
+        
+        System.out.println();
+        
+        // sort array
+        
+        QuickSort.qsort(a);
+        
+        System.out.print("Sorted array: ");
+        for(i=0; i < a.length; i++)
+            System.out.print(a[i]);
 	}
 }
