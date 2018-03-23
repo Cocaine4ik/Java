8030837213f4878251bdfc56c138d194a1b2aa74
Alex <Alex@DESKTOP-JIN5V77>
2018-03-23 11:57:49 +0200

    upd

--- Exercises/src/MethodsAndClasses/QSDemo.java
+++ Exercises/src/MethodsAndClasses/QSDemo.java
@@ -1,5 +1,13 @@
 package MethodsAndClasses;
 
 public class QSDemo {
-
+	public static void main(String args[]) {
+		
+		char a[] = {'d', 'x', 'a', 'r', 'p', 'j', 'i'};
+		int i;
+		
+		System.out.print("Original array: ");
+        for(i=0; i < a.length; i++)
+            System.out.print(a[i]) ;
+	}
 }
