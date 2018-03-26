a06db1b161909d279d326e940d7eee4d894d991f
Alex <Alex@DESKTOP-JIN5V77>
2018-03-26 17:54:48 +0300

    demonstration methods with changeable length

---
+++ Examples/src/MethodsAndClasses/VarArgs.java
@@ -0,0 +1,25 @@
+package MethodsAndClasses;
+
+// demonstration methods with changeable length
+
+public class VarArgs {
+
+// Method VaTest() with arguments of changeable length
+	
+	static void vaTest(int ...v) {
+		System.out.println("Number of args: " + v.length);
+		System.out.println("Contents: ");	
+		
+	for(int i = 0; i < v.length; i++)
+		System.out.println(" arg " + i + ": " + v[i]);
+		
+		System.out.println();
+	}
+	public static void main(String args[])
+    {
+        // Method vaTest()   can be call with changeable number of arguments
+        vaTest(10); // 1 argument
+        vaTest(1, 2, 3); // few argument
+        vaTest(); // no arguments
+    }
+}
