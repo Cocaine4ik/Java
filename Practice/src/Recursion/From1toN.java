30c5f3fd6824dfdc0a7d3361894ea42ffc71fa26
Alex <Alex@DESKTOP-JIN5V77>
2018-03-29 14:49:03 +0300

    We have a natural number - n. Enter all numbers from 1 to n.

--- Practice/src/Recursion/From1toN.java
+++ Practice/src/Recursion/From1toN.java
@@ -1,5 +1,32 @@
 package Recursion;
 
+import java.util.Scanner;
+
+// We have a natural number - n. Enter all numbers from 1 to n.
+
 public class From1toN {
 
+	static int zero = 0;
+
+	static public void OneToN(int n) {
+		// base case		
+		if(zero == n) {
+			return;
+		}
+		// recursion step	
+		zero++;
+		System.out.print(zero + " ");
+		OneToN(n);
+	}
+	
+	public static void main(String args[]) {
+		
+		Scanner in = new Scanner(System.in);
+		From1toN a = new From1toN();
+		
+		System.out.print("n = ");
+		int n = in.nextInt();
+		
+		a.OneToN(n);
+	}
 }
