f4a44218f3047c707b87dc0c80da5c30edd805d0
Alex <Alex@DESKTOP-JIN5V77>
2018-03-30 16:23:20 +0300

    upd

--- Practice/src/Recursion/AkkermanFunc.java
+++ Practice/src/Recursion/AkkermanFunc.java
@@ -7,19 +7,34 @@ import java.util.Scanner;
 public class AkkermanFunc {
 	
 	public static int Afunc(int m, int n) {
-        // Базовый случай
+        // Base case	
         if (m == 0) {
             return n + 1;
-        } // Шаг рекурсии / рекурсивное условие
+        } // Recursion step
         else if (n == 0 && m > 0) {
             return Afunc(m - 1, 1);
-        } // Шаг рекурсии / рекурсивное условие
+        } //Recursion step #2
         else {
             return Afunc(m - 1, Afunc(m, n - 1));
         }
     }
     public static void main(String[] args) {
     	
-        System.out.println(Afunc(9, 12)); // вызов рекурсивной функции
+    	Scanner in = new Scanner(System.in);
+    	AkkermanFunc A = new AkkermanFunc();
+    	int m, n;
+    	
+    	// check input if m >= 4
+    	
+    	do {System.out.print("Enter m (m can't 4 or more): ");
+    		m = in.nextInt();
+    	}
+    	while(m >= 4);
+    	
+    	System.out.print("Enter n: ");
+    	
+    	n = in.nextInt();
+    	
+        System.out.println("Afunc is: " + Afunc(m, n)); 
     }
 }
