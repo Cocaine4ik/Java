cdd45954af38268619c6e1b0175701ad367b16ea
Alex <Alex@DESKTOP-JIN5V77>
2018-04-02 14:43:20 +0300

    class Triangle extend from class TwoDShape

---
+++ Examples/src/Inheritance/Triangle.java
@@ -0,0 +1,20 @@
+package Inheritance;
+
+// Class TwoDShape subclass for showing triangles.
+// Class Triangle extend from class TwoDShape
+
+public class Triangle extends TwoDShape {
+
+	String style;
+	
+	double area() {
+		// From the class Triangle we can apply members of the class
+        // TwoDShape in the same way as for their own members.
+		return width * height / 2;
+	}
+	
+	void showStyle() {
+		System.out.println("Triangle is + " + style);
+	}
+	
+}
