65ee5fcb54023c9781820e55f5c73262e950769d
Alex <Alex@DESKTOP-JIN5V77>
2018-04-02 12:08:45 +0300

    class which describes 2-dimensional objects

---
+++ Examples/src/Inheritance/TwoDShape.java
@@ -0,0 +1,14 @@
+package Inheritance;
+
+// simple classes hierarchy
+// class which describes 2-dimensional objects
+
+public class TwoDShape {
+
+	double width;
+	double height;
+	
+	void showDim() {
+		System.out.println("Width and height are " + width + "and" + height);
+	}
+}
