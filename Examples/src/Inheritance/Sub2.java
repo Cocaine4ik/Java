8a5067f1817761fbf040d518a2e7c1e4f73acc1a
Alex <Alex@192.168.1.3>
2018-04-04 16:34:52 +0300

    upd

--- Examples/src/Inheritance/Sub2.java
+++ Examples/src/Inheritance/Sub2.java
@@ -2,7 +2,7 @@ package Inheritance;
 
 // Dynamic dispatching methods demonstration #3
 
-public class Sub2 {
+public class Sub2 extends Sup {
 	void who() {
 		System.out.println("Who() in Sub2");
 	}
