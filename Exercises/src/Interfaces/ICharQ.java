17722fb7485b8606094a37b4432f52d08dd4a7ce
Alex <Alex@DESKTOP-JIN5V77>
2018-04-10 15:28:49 +0300

    interface for character queue

---
+++ Exercises/src/Interfaces/ICharQ.java
@@ -0,0 +1,12 @@
+package Interfaces;
+
+// interface for character queue
+
+public interface ICharQ {
+	
+	// put symbol to queue
+	void put(char ch);
+	
+	// get symbol from the queue
+	char get();
+}
