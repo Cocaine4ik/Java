c5b9dc8286ebc896d295058fb4963a180690f65f
Alex <Alex@DESKTOP-JIN5V77>
2018-04-10 13:56:22 +0300

    interface Series

---
+++ Examples/src/PackagesAndInterfaces/Interfaces/Series.java
@@ -0,0 +1,7 @@
+package PackagesAndInterfaces.Interfaces;
+
+public interface Series {
+	int getNext(); // return next number
+	void reset(); // start from the beginning 
+	void setStart(int õ); // set beginner value
+}
