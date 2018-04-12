957f49c6d5abcebd347fda2cc14794b298d1457e
Alex <Alex@DESKTOP-JIN5V77>
2018-04-12 17:26:18 +0300

    upd

--- Exercises/src/Interfaces/CircularQueue.java
+++ Exercises/src/Interfaces/CircularQueue.java
@@ -1,11 +1,20 @@
 package Interfaces;
 
-public class CircularQueue implements ICharQ {
+// Circular Queue
 
-	@Override
+public class CircularQueue extends Queue implements ICharQ {
+
+	public CircularQueue(int size) {
+		super(size);
+	}
+
 	public void put(char ch) {
-		// TODO Auto-generated method stub
 		
+		/* Queue being full, if index putloc per unit
+        lower the index getloc or if index putloc pointing
+        to the end of array, and index getloc - to the beginning. */
+		
+		if(putloc + 1 == getloc | ((putloc == q.length - 1) ))
 	}
 
 	@Override
