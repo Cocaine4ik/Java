177189ad0e1e1ab780fc6732892c7e45447aa7e6
Cocaine <Cocaine@DESKTOP-QDQILMQ>
2018-04-15 16:12:39 +0300

    Dynamic queue

---
+++ Exercises/src/Interfaces/DynQueue.java
@@ -0,0 +1,35 @@
+package Interfaces;
+
+// Dynamic queue
+
+public class DynQueue extends Queue implements ICharQ {
+
+	public DynQueue(int size) {
+		super(size);
+	}
+
+	public void put(char ch) {
+		if(putloc == q.length-1) {
+			// increase queue size
+			char t[] = new char [q.length * 2];
+			
+			// copy elements to the new queue
+			for(int i = 0; i < q.length; i++)
+				t[i] = q[i];
+			
+			q = t;
+		}
+		putloc++;
+		q[putloc] = ch;
+	}
+
+	public char get() {
+		if(getloc == putloc) {
+			System.out.println(" - Queue is empty.");
+			return (char) 0;
+		}
+		getloc++;
+		return q[getloc];
+	}
+
+}
