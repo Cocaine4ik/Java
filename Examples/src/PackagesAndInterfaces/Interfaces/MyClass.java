ad2ed00d46fa79fa3681b32c6194d65dc76df5aa
Alex <Alex@DESKTOP-JIN5V77>
2018-04-17 15:54:56 +0300

    this class must rannounced in interfaces A and Bealized all methods

---
+++ Examples/src/PackagesAndInterfaces/Interfaces/MyClass.java
@@ -0,0 +1,20 @@
+package PackagesAndInterfaces.Interfaces;
+
+// this class must realized all methods
+// announced in interfaces A and B
+
+public class MyClass implements B {
+
+	public void meth1() {
+		System.out.println("Implement methl().");		
+	}
+
+	public void meth2() {
+		System.out.println("Implement meth2().");		
+	}
+
+	public void meth3() {
+		System.out.println("Implement meth3().");	
+	}
+
+}
