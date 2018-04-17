84854de989c2c4a802f130b8a450653bbefbd8bc
Alex <Alex@DESKTOP-JIN5V77>
2018-04-17 15:51:32 +0300

    interface b includes meth1() and meth2(), after all he add meth3().

---
+++ Examples/src/PackagesAndInterfaces/Interfaces/B.java
@@ -0,0 +1,11 @@
+package PackagesAndInterfaces.Interfaces;
+
+// interface b includes meth1() and meth2(), 
+// after all he add meth3().
+
+public interface B extends A {
+
+	// interface B extends interface A
+	
+	void meth3();
+}
