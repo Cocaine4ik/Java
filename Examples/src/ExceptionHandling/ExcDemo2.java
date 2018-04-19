a33c23f864b528fd1f47c4f180d9f51a95a8998a
Alex <Alex@DESKTOP-JIN5V77>
2018-04-19 16:58:47 +0300

    Demonstration ExcTest

---
+++ Examples/src/ExceptionHandling/ExcDemo2.java
@@ -0,0 +1,16 @@
+package ExceptionHandling;
+
+// Demonstration ExcTest
+
+public class ExcDemo2 {
+	public static void main(String args[]) {
+		try {
+			ExcTest.genException();
+		}
+		catch(ArrayIndexOutOfBoundsException exc) {
+			System.out.println("Index out-of-bounds!");
+		}
+		System.out.println("After catch statement.");
+	}
+	
+}
