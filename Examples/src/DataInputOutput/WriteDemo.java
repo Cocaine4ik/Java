3ec4a78f80efed2d26b5db76af9c09ccc53aad51
Alex <Alex@192.168.1.3>
2018-04-27 12:53:31 +0300

    usage method System.out.write()

---
+++ Examples/src/DataInputOutput/WriteDemo.java
@@ -0,0 +1,15 @@
+package DataInputOutput;
+
+// usage method System.out.write()
+
+public class WriteDemo {
+	public static void main(String args[]) {
+		int b;
+		
+		b = 'S';
+		
+		// output bytes on screen
+		System.out.write(b);
+		System.out.write('\n');
+	}
+}
