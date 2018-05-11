6bd7aab1b5944cb68c8bf24c2b25185ff5cbe178
Cocaine4ik <kokotkin.alexander@gmail.com>
2018-05-11 13:58:38 +0300

    upd

--- Examples/src/DataInputOutput/Ktod.java
+++ Examples/src/DataInputOutput/Ktod.java
@@ -18,11 +18,12 @@ public class Ktod {
 		try (FileWriter fw = new FileWriter("test.txt")) {
 			do {System.out.print(": ");
 			str = br.readLine();
+			if(str.compareTo("stop") == 0) break;
 				str = str + "\r\n"; // add new line
 			// writing text lines in to file
 				fw.write(str);
 			}
-			while(str.compareTo("stop") != 0);
+			while(str.compareTo("q") != 0);
 		}
 		catch (IOException exc) {
 			System.out.println("I/O Error: " + exc);
