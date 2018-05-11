cf38ae99078d21db26ec44a6d5924cad73e32994
Cocaine4ik <kokotkin.alexander@gmail.com>
2018-05-11 13:32:36 +0300

    Simple example utility if input from keyboard and data output to  disk

---
+++ Examples/src/DataInputOutput/Ktod.java
@@ -0,0 +1,31 @@
+package DataInputOutput;
+
+// Simple example utility if input from keyboard and data output
+// to disk, demonstrating usage class FileWriter.
+
+import java.io.*;
+
+public class Ktod {
+	public static void main(String args[]) {
+		
+		String str;
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("Enter text('q' to quit).");
+		
+		// creating data input type FileWriter
+		try (FileWriter fw = new FileWriter("test.txt")) {
+			do {System.out.print(": ");
+			str = br.readLine();
+				str = str + "\r\n"; // add new line
+			// writing text lines in to file
+				fw.write(str);
+			}
+			while(str.compareTo("q") == 0);
+		}
+		catch (IOException exc) {
+			System.out.println("I/O Error: " + exc);
+		}
+	}
+}
