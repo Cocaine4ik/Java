b00cd7768d05837246e7f575a26cda4c65b08bc4
Cocaine4ik <kokotkin.alexander@gmail.com>
2018-05-11 14:33:14 +0300

    Simple utility input from disk and output to screen

--- Examples/src/DataInputOutput/DtoS.java
+++ Examples/src/DataInputOutput/DtoS.java
@@ -6,5 +6,21 @@ package DataInputOutput;
 import java.io.*;
 
 public class DtoS {
-
+	public static void main(String args[]) {
+		
+		String s;
+		
+		// Creating in class BufferedReader a sheath in with target to conclude in
+	    // class FileReader and organize data reading from fail.
+		
+		try(BufferedReader br = new BufferedReader(new FileReader("test.txt"))) {
+			
+			while((s = br.readLine()) != null) {
+				System.out.println(s);
+			}
+		}
+		catch (IOException exc) {
+			System.out.println("I/O Error: " + exc);
+		}
+	}
 }
