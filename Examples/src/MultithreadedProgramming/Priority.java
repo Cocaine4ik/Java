2a4991b15f3486540c53e0a4b9e6be0a62f0575d
Cocaine4ik <kokotkin.alexander@gmail.com>
2018-05-17 15:18:38 +0300

    Demonstration threads with different priority

---
+++ Examples/src/MultithreadedProgramming/Priority.java
@@ -0,0 +1,41 @@
+package MultithreadedProgramming;
+
+// Demonstration threads with different priority
+
+public class Priority implements Runnable {
+	int count;
+	Thread thrd;
+	
+	static boolean stop = false;
+	static String currentName;
+	
+    /* Building new thread. Pay attention that 
+    construct isn't starting thread executing */
+	Priority(String name) {
+		thrd = new Thread(this, name);
+		count = 0;
+		currentName = name;
+	}
+
+	// start executing new thread
+	public void run() {
+		
+		System.out.println(thrd.getName() + " starting.");
+		
+		do {
+			count++;
+			if(currentName.compareTo(thrd.getName()) != 0) {
+				currentName = thrd.getName();
+				System.out.println("In " + currentName);
+			}
+			// first thread which will achieve value 10000000
+			// will end else threads
+		}
+		while (stop == false && count == 10000000);
+		stop = true;
+		
+        System.out.println("\n" + thrd.getName() +
+                " terminating.");
+		
+	}
+}
