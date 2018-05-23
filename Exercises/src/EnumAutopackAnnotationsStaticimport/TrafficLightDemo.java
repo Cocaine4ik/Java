03be5f695457e256cf10a31136f20f9aead8bc40
Cocaine4ik <kokotkin.alexander@gmail.com>
2018-05-23 12:04:21 +0300

    Traffic Light demonstration

--- Exercises/src/EnumAutopackAnnotationsStaticimport/TrafficLightDemo.java
+++ Exercises/src/EnumAutopackAnnotationsStaticimport/TrafficLightDemo.java
@@ -1,9 +1,20 @@
 package EnumAutopackAnnotationsStaticimport;
 
+// Traffic Light demonstration
+
 enum TrafficLightColor {
 	RED, GREEN, YELLOW
 }
 
 public class TrafficLightDemo {
-	
+	public static void main(String args[]) {
+		
+		TrafficLightSimulator t1 = new TrafficLightSimulator(TrafficLightColor.GREEN);
+		
+		for(int i = 0; i < 9; i++) {
+			System.out.println(t1.getColor());
+			t1.waitForChange();
+		}
+		t1.cancel();
+	}
 }
