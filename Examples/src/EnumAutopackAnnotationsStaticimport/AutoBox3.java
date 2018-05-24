bc99ffdbfc78ec2766685e6cb692bf9a28bc7469
Cocaine4ik <kokotkin.alexander@gmail.com>
2018-05-24 12:06:37 +0300

    autoboing and autounboxing in statements

--- Examples/src/EnumAutopackAnnotationsStaticimport/AutoBox3.java
+++ Examples/src/EnumAutopackAnnotationsStaticimport/AutoBox3.java
@@ -25,6 +25,12 @@ public class AutoBox3 {
 		
 		// In the next statement object iOb is autounboxing, calculating,
 		// and result is autoboxing to object iOb.
+		iOb2 = iOb + (iOb / 3);
+		System.out.println("iOb afte expressions: " + iOb2);
 		
+		// But in this case caculating the same staement,
+		// but repeated boxing isn't making
+		i = iOb + (iOb / 3);
+		System.out.println("i after expressions: " + i);
 	}
 }
