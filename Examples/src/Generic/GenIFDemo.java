574231bb8e70c7d10f7ec5a56b64b364932890bf
Cocaine4ik <kokotkin.alexander@gmail.com>
2018-06-01 15:19:36 +0300

    demo for containment interface

--- Examples/src/Generic/GenIFDemo.java
+++ Examples/src/Generic/GenIFDemo.java
@@ -9,5 +9,22 @@ public class GenIFDemo {
 		
 		MyClass<Integer> ob = new MyClass<Integer>(x);
 		
+		if(ob.contains(2))
+			System.out.println("2 is in ob");
+		else
+			System.out.println("2 is NOT in ob");
+		
+		if(ob.contains(5))
+			System.out.println("5 is in ob");
+		else
+			System.out.println("2 is NOT in ob");
+		
+		// next string of code are impossible because object
+		// a variant of implement interface Containment for
+		// type Integer, and a value 9.25 concerns to type Double.
+		// if(ob.contains(9.25)) // Impossible!!!
+		// System.out.println("9.25 is in ob"); ~
+		
+		
 	}
 }
