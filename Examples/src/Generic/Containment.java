d565322d6ba6abbecf59c5b89343e4ddf94453d9
Cocaine4ik <kokotkin.alexander@gmail.com>
2018-06-01 15:12:44 +0300

    upd

--- Examples/src/Generic/Containment.java
+++ Examples/src/Generic/Containment.java
@@ -10,26 +10,4 @@ public interface Containment<T> { // generic interface
 	// method contain is checking, is contain some element
 	// in class object which implement interface Containment
 	boolean contains(T o);
-		
-		// implement interface Containment through array,
-		// used for values storage.
-		// Any class, implemented simple interface
-		// must be generic too
-		class MyClass<T> implements Containment<T> {
-
-			T[] arrayRef;
-			
-			MyClass(T[] o) {
-				arrayRef = o;
-			}
-			
-			// implement method contains()
-			public boolean contains(T o) {
-				for(T x : arrayRef)
-					if(x.equals(o)) return true;
-				return false;
-			}
-			
-		}
-
 }
---
+++ Examples/src/Generic/GenIFDemo.java
@@ -0,0 +1,13 @@
+package Generic;
+
+// demo for containment interface
+
+public class GenIFDemo {
+	public static void main(String args[]) {
+		
+		Integer x[] = { 1, 2, 3 };
+		
+		MyClass<Integer> ob = new MyClass<Integer>(x);
+		
+	}
+}
