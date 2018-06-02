895fbc5818e3a11bf5258fbf2ad716341caabbb5
Cocaine <Cocaine@DESKTOP-QDQILMQ>
2018-06-02 10:31:45 +0300

    exception in connection with errors of queue overflow

--- Exercises/src/Generic/QueueFullException.java
+++ Exercises/src/Generic/QueueFullException.java
@@ -1,5 +1,14 @@
 package Generic;
 
-public class QueueFullException extends {
+// exception in connection with errors of queue overflow
 
+public class QueueFullException extends Exception {
+
+	int size;
+	
+	QueueFullException(int s) { s = size; }
+	
+	public String toString() {
+		return "\nQueue is full. Maximum size " +size;
+	}
 }
