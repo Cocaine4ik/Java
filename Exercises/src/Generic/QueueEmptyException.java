ff61de006aaad099ce855d7cb115be054234a067
Cocaine <Cocaine@DESKTOP-QDQILMQ>
2018-06-02 10:36:52 +0300

    exception in connection with errors of queue empty

---
+++ Exercises/src/Generic/QueueEmptyException.java
@@ -0,0 +1,10 @@
+package Generic;
+
+// exception in connection with errors of queue empty
+
+public class QueueEmptyException {
+	
+	public String toString() {
+		return "\nQueue is empty.";
+	}
+}
