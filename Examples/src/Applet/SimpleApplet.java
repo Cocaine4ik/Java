6c220e52a802bf61c1c767d790a3143ba9fbd3e2
Cocaine4ik <kokotkin.alexander@gmail.com>
2018-06-05 17:43:15 +0300

    upd

--- Examples/src/Applet/SimpleApplet.java
+++ Examples/src/Applet/SimpleApplet.java
@@ -5,7 +5,7 @@ package Applet;
 import java.awt.*;
 import java.applet.*;
 
-public class SimpleApplet {
+public class SimpleApplet extends Applet{
 	
 	public void paint(Graphics g) {
 		// enter character string in applet window
