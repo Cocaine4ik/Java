8344009f2c80aa27283cd165041fc0107e25f9ef
Cocaine4ik <kokotkin.alexander@gmail.com>
2018-06-05 17:26:20 +0300

    applet with a minimal function opportunities

---
+++ Examples/src/Applet/SimpleApplet.java
@@ -0,0 +1,14 @@
+package Applet;
+
+// applet with a minimal function opportunities 
+
+import java.awt.*;
+import java.applet.*;
+
+public class SimpleApplet {
+	
+	public void paint(Graphics g) {
+		// enter character string in applet window
+		g.drawString("Java makes applets easy.", 20, 20);
+	}
+}
