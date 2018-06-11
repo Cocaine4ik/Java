8d52c7986f706b8c8b80a458ae7f769e633d7059
Cocaine4ik <kokotkin.alexander@gmail.com>
2018-06-11 16:50:23 +0300

    // add parameters option

--- Exercises/src/Applet/Banner.java
+++ Exercises/src/Applet/Banner.java
@@ -12,18 +12,35 @@ from tight to left in applet window.
 import java.awt.*;
 import java.applet.*;
 
-/* <applet code="Banner" width=300 height=50>
-   </applet>
+/* <applet code = "Banner" width=300 height=50>
+	<param name = message value = " I like Java! ">
+	<param name = delay value = 500>
+	</applet>
 */
 
 public class Banner extends Applet implements Runnable {
-	String msg = " Java Rules the Web ";
+	String msg;
+	int delay;
 	Thread t;
 	boolean stopFlag;
 
 	// initial variable t with empty value null
 	public void init() {
+		String temp;
+		msg = getParameter("message");
+		if(msg == null) msg = "Java Rules the Web";
+		temp = getParameter("delay");
+		try {
+			if(temp != null)
+				delay = Integer.parseInt(temp);
+			else 
+				delay = 250; // if paramater doesn't exist used default value.
+		}
+		catch(NumberFormatException exc) {
+			delay = 250; // if error is occurred we are using default value
+		}
 		t = null;
+		
 	}
 	
 	// start thread
@@ -36,11 +53,15 @@ public class Banner extends Applet implements Runnable {
 	
 	// enter point to thread manipulate a big banner
 	public void run() {
+		char ch;
 		// show big banner again
 		for( ; ; ) {
 			try { 
 				repaint();
-				Thread.sleep(650);
+				Thread.sleep(delay);
+				ch = msg.charAt(0);
+				msg = msg.substring(1, msg.length());
+				msg += ch;
 				if(stopFlag)
 					break;
 			}
@@ -56,10 +77,6 @@ public class Banner extends Applet implements Runnable {
 	
 	// show big banner
 	public void paint(Graphics g) {
-		char ch;
-		ch = msg.charAt(0);
-		msg = msg.substring(1, msg.length());
-		msg += ch;
 		g.drawString(msg, 50, 30);
 	}
 	
