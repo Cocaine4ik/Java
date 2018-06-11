f30da285e91c9f029558434fafed3973b89548d3
Cocaine4ik <kokotkin.alexander@gmail.com>
2018-06-11 17:33:20 +0300

    simple clock applet

---
+++ Homeworks/src/Applet/Clock.java
@@ -0,0 +1,55 @@
+package Applet;
+
+import java.util.*;
+import java.awt.*;
+import java.applet.*;
+
+// This applet must show current time, refresh
+// content of the current window every second.
+// Use object from the class Calendar and his methods.
+
+public class Clock extends Applet implements Runnable {
+	
+	String msg;
+	Calendar clock;
+	Thread t;
+	boolean stopFlag;
+	
+	// initialize applet
+	public void init() {
+		t = null;
+		msg = null;
+	}
+	
+	// start thread
+	public void start() {
+		t = new Thread(this);
+		stopFlag = false;
+		t.start();
+	}
+
+	public void run() {
+		for( ; ; ) {
+			try {
+				repaint();
+				Thread.sleep(1000);
+				if(stopFlag)
+					break;
+				
+			}
+			catch(InterruptedException exc) {}
+		}
+	}
+	
+	public void stop() {
+		stopFlag = true;
+		t = null;
+	}
+	
+	public void paint(Graphics g) {
+		clock = Calendar.getInstance();
+		msg = "Current time: " + clock.HOUR + " " + clock.MINUTE + " " + clock.SECOND;
+		g.drawString(msg, 50, 30);
+	}
+	
+}
